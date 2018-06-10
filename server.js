af973df8eb997b1bfc3fc8db48da05c65a8dd128
admin אביגיל הרמתי <dreckguy@gmail.com>
2018-06-10 15:16:55 +0300

    sending static uploads

---
+++ server.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+
+var uploads = [
+    {title:'Maddona - like a virgin', url:'https://www.youtube.com/watch?v=s__rX_WL100'},
+    {title:'Offsprinfg - Why don\'t you get a job?', url:'https://www.youtube.com/watch?v=LH-i8IvYIcg'}  
+];
+
+app.get('/uploads', function(req, res){
+    res.json(uploads);
+});
+
+
+
+app.listen(PORT, function() {
+    console.log(`listenning on ${PORT}`);
+  })
