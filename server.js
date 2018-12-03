ecf95757da47af4b27c1e6f2376215e308c46912
ophir <devteam@wizer.me>
2018-12-03 09:57:53 +0200

    remove the need for title

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const getYoutubeTitle = require('get-youtube-title');
+
 
 const db = require('./db');
-const storage = require('./storage');
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,12 +26,15 @@ app.get('/uploads', function(req, res){
 app.post('/upload',function(req,res){
 
     let song = req.body;
+    if(!song.title){
+        getYoutubeTitle(song.id, function (err, title) {
+            if(!err){
+                song.title = title;
+            }
+          });
+    }
     db.insert(song,()=>{
         res.json(song);
-        storage.upload(song, ()=> {
-            console.log('uploaded!');
-
-        });
     });
     
 });
